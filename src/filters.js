e41cb591ba5c9545f40fee9d14df2fa7ff9249b2
santiagoRicops <santuago231@gmail.com>
2023-11-12 14:59:25 -0500

    se agrego el metodo searcher

--- src/filters.js
+++ src/filters.js
@@ -1,20 +1,21 @@
-
 export const filter = {
   A_Z(pokemons) {
-   const filterA_Z =  pokemons.sort((a, b) => a.name.localeCompare(b.name))
-   
-   return filterA_Z
-    
+    const filterA_Z = pokemons.sort((a, b) => a.name.localeCompare(b.name))
+
+    return filterA_Z
   },
   Z_A(pokemons) {
-    const filterZ_A = pokemons.sort((a, b)=>  b.name.localeCompare(a.name))
+    const filterZ_A = pokemons.sort((a, b) => b.name.localeCompare(a.name))
     return filterZ_A
-  }, 
-  searcherButton(){
+  },
+  searcher(pokemonsArry, input) {
+    const searcherPokemos = pokemonsArry.filter(
+      (nameFilterPokemons) =>
+        nameFilterPokemons.name.toLowerCase() === input.value.toLowerCase(),
+    )
+    return searcherPokemos
+  },
+  /*  searcherButton(){
     const searcherButtonNormal = searcherPokemonTypeNormal.filter((TypePokemonsNormal)=> TypePokemonsNormal.type === '')
-  }
-  ,searcher(pokemonsArry, input){
-  const searcherPokemos =  pokemonsArry.filter((nameFilterPokemons)=> nameFilterPokemons.name === input.value)
-   return  searcherPokemos
-  }
+  }*/
 }
